ee09392085231bb38564c197173520122cd8072b
rover-t <trevor.ortega5@gmail.com>
2021-03-17 13:28:59 -0700

    Updated processing based on Antoni's work seems more accurate.

--- tesseract-examples/preprocessed_boxes.py
+++ tesseract-examples/preprocessed_boxes.py
@@ -2,12 +2,12 @@ import cv2
 import pytesseract
 
 #imports a test image I scaned of my handwriting
-img = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
+original = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
 
 #does some filtering to improve quality
 #more research needed to find most efective filters for use case
-img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-img = cv2.bilateralFilter(img,9,75,75)
+resized = cv2.resize(original, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+img = cv2.threshold(resized, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 # get dimensions of image
 h, w = img.shape
@@ -22,7 +22,7 @@ for b in boxes.splitlines():
     #add boxes to main image
     img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
     #create and show a cropped image of each character
-    cropped = img[h-int(b[4]):h-int(b[2]),int(b[1]):int(b[3])]
+    cropped = resized[h-int(b[4]):h-int(b[2]),int(b[1]):int(b[3])]
     cv2.imshow(b[0], cropped)
 
 #show full image with boxes
