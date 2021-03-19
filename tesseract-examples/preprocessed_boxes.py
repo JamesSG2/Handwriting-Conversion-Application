f657a91dc993d1499fd0c6ea30a92f07839611bd
rover-t <trevor.ortega5@gmail.com>
2021-03-18 23:45:40 -0700

    added tougher images and messed with variables

--- tesseract-examples/preprocessed_boxes.py
+++ tesseract-examples/preprocessed_boxes.py
@@ -2,11 +2,11 @@ import cv2
 import pytesseract
 
 # imports a test image I scaned of my handwriting
-original = cv2.imread('images\\track.png', cv2.IMREAD_GRAYSCALE)
+original = cv2.imread('images\\Track.png', cv2.IMREAD_GRAYSCALE)
 
 # does some filtering to improve quality
 # more research needed to find most efective filters for use case
-resized = cv2.resize(original, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+resized = cv2.resize(original, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 # kernel = np.ones((1, 1), np.uint8)
 # img = cv2.dilate(img, kernel, iterations=1)
 # img = cv2.erode(img, kernel, iterations=1)
