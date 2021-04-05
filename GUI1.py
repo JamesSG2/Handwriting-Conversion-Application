4463d9f936c605ae950e72e69b4956f18eae2eec
JosephHaugen <jghaugen@icloud.com>
2021-04-04 17:06:52 -0700

    Update GUI1.py

--- GUI1.py
+++ GUI1.py
@@ -1,22 +1,20 @@
 #Just a very basic GUI. Still very messy but I figured I would upload
 #just in case.
 
-
-import tkinter as tk
+#tkinter is pre-installed with python to make very basic GUI's
+from tkinter import *
 from tkinter import filedialog
-#from PIL import ImageTk, Image
-
+root = Tk()
+#Function gets the file path and prints it in the command
+#prompt as well as on the screen.
 def UploadAction(event=None):
     filename = filedialog.askopenfilename()
     print('Selected:', filename)
+    myLabel1 = Label(root, text=filename)
+    myLabel1.pack()
 
-root = tk.Tk()
-button = tk.Button(root, text='Open', command=UploadAction)
+#very simple button that calls the upload action Function
+button = Button(root, text='Upload an image', command=UploadAction)
 button.pack()
 
-#image1 = Image.open(r"C:\Users\jghau\Downloads/Img.png")
-#test = ImageTk.PhotoImage(image1)
-
-#label1 = tkinter.Label(image=test)
-#label1.image = test
 root.mainloop()
