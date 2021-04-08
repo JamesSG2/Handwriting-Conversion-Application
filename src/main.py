a6949cad51191fc6d09e019ef6527d28dd193d6d
trevor <trevor.ortega5@gmail.com>
2021-04-07 20:53:42 -0700

    Updated main.py to conform to style guides regarding line length.

--- src/main.py
+++ src/main.py
@@ -22,7 +22,8 @@ def new_full_correction(text_to_correct, correct_text):
             patterns.append(search)
             patterns.append(segment)
         for p in range(0, len(patterns), 2):
-            text_to_correct = re.sub(patterns[p], patterns[p+1], text_to_correct)
+            text_to_correct = re.sub(patterns[p], patterns[p+1], \
+            text_to_correct)
         # print(str(k) + ":" + text_to_correct)
     return text_to_correct
 
@@ -53,7 +54,8 @@ def punctuation_analysis(img, text_contents):
     cleaned = cv2.add(img, newmask)
 
     # find contours
-    contours_full, hierarchy = cv2.findContours(dialated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours_full, hierarchy = cv2.findContours(dialated,cv2.RETR_TREE, \
+                                                cv2.CHAIN_APPROX_SIMPLE)
     contours = contours_full[::2]
 
     # place a bounding box around each contour
@@ -65,7 +67,10 @@ def punctuation_analysis(img, text_contents):
     # crop boxes and save
     image_list = []
     for i in range(len(contours)):
-        cropped = cleaned[int(boundRect[i][1]):int(boundRect[i][1]+boundRect[i][3]),int(boundRect[i][0]):int(boundRect[i][0]+boundRect[i][2])]
+        cropped = cleaned[int(boundRect[i][1]):int(boundRect[i][1] \
+            +boundRect[i][3]), \
+            int(boundRect[i][0]):int(boundRect[i][0]+boundRect[i][2])]
+
         ordinal = "ord_" + str(ord(text_contents[i]))
         storage_list = [cropped, ordinal]
         image_list.append(storage_list)
@@ -74,7 +79,8 @@ def punctuation_analysis(img, text_contents):
     for i in range(len(contours)):
         color = (0, 0, 0)
         cv2.rectangle(cleaned, (int(boundRect[i][0]), int(boundRect[i][1])), \
-            (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+            (int(boundRect[i][0]+boundRect[i][2]), \
+            int(boundRect[i][1]+boundRect[i][3])), color, 2)
 
     return cleaned, image_list
 
@@ -82,7 +88,8 @@ def character_analysis(img, text_contents):
     # make image binary and scale up
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     resized = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    threshold = cv2.threshold(resized, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    threshold = cv2.threshold(resized, 0, 255, \
+        cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     h, w, = threshold.shape
 
     # create 2D list and string for storing characters and bounding boxes
@@ -105,10 +112,12 @@ def character_analysis(img, text_contents):
     image_list = []
     for i in range(len(text_array)):
         # crop the image based on tesseract bounding boxes
-        cropped = resized[h-int(text_array[i][4]):h-int(text_array[i][2]),int(text_array[i][1]):int(text_array[i][3])]
+        cropped = resized[h-int(text_array[i][4]):h-int(text_array[i][2]), \
+            int(text_array[i][1]):int(text_array[i][3])]
         # get the unicode number of the character and check if it is a letter
         char_num = ord(text_array[i][0])
-        if(((char_num>=65) and (char_num<=90)) or ((char_num>=97) and (char_num<=122))):
+        if(((char_num>=65) and (char_num<=90)) \
+            or ((char_num>=97) and (char_num<=122))):
             # store cropped image and it's character in a nested list
             storage_list = [cropped, text_string[i]]
             image_list.append(storage_list)
@@ -159,8 +168,10 @@ def analyze_sample(name, target):
             pass
         # check how many images are already in folder
         char_count = len(os.listdir('output\\' + name + '\\' + character_id))
-        # save the image with the number of images already in the folder appended to avoid duplicates
-        location = 'output\\' + name + '\\' + character_id + '\\' + character_id + "_" + str(char_count) + '.png'
+        # save the image with the number of images already in the folder
+        # appended to avoid duplicates
+        location = 'output\\' + name + '\\' + character_id + '\\' + \
+            character_id + "_" + str(char_count) + '.png'
         cv2.imwrite(location, i[0])
 
     print("Saved")
@@ -178,7 +189,8 @@ def hconcat_resize(img_list, interpolation = cv2.INTER_CUBIC):
     h_size = min(img.shape[0] for img in img_list)
 
     # image resizing
-    im_list_resize = [cv2.resize(img, (int(img.shape[1] * h_size / img.shape[0]), h_size), interpolation = interpolation) for img in img_list]
+    im_list_resize = [cv2.resize(img, (int(img.shape[1]*h_size/img.shape[0]), \
+        h_size), interpolation = interpolation) for img in img_list]
 
     # return final image
     return cv2.hconcat(im_list_resize)
@@ -204,9 +216,11 @@ def output_handwriting(name, phrase):
         character_id = char + "_" + case
         char_num = ord(char)
 
-        if(((char_num>=65) and (char_num<=90)) or ((char_num>=97) and (char_num<=122))):
+        if(((char_num>=65) and (char_num<=90)) \
+            or ((char_num>=97) and (char_num<=122))):
             try:
-                char_count = len(os.listdir('output\\' + name + '\\' + character_id))
+                char_count = len(os.listdir('output\\' + name + '\\' \
+                    + character_id))
             except FileNotFoundError:
                 print("could not find letter:" + char)
                 continue
@@ -215,7 +229,8 @@ def output_handwriting(name, phrase):
                 continue
 
             char_select = random.randint(0, char_count-1)
-            char_img = cv2.imread('output\\' + name + '\\' + character_id + '\\' + character_id + "_" + str(char_select) + '.png')
+            char_img = cv2.imread('output\\' + name + '\\' + character_id \
+                + '\\' + character_id + "_" + str(char_select) + '.png')
             output_image = add_char(output_image, char_img)
 
         elif(char_num==32):
