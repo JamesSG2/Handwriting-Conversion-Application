b4ede770c8d294689c414a0fd33e58a24465bec2
BigWub67 <antonianf03@gmail.com>
2021-04-20 20:02:08 -0700

    added functionality to uploadedDataScreen and made changes (removed spell check) to backend.py to make it work

--- src/GUI/uploadDataScreen.py
+++ src/GUI/uploadDataScreen.py
@@ -1,13 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 import os
+import backend
 
 class UploadDataScreen(tk.Frame):
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent)
         self.parent = parent
+        self.controller = controller
         # List of all profiles (folders) in output
         self.profileList = controller.profileList
+        self.files = ""
         # makes widgets
         self.createWidgets(controller)
         # place widgets onto frame
@@ -17,6 +21,26 @@ class UploadDataScreen(tk.Frame):
 
     def createWidgets(self, controller):
         self.backButton = ttk.Button(self, text = "Back", command = lambda: controller.showFrame("ProfileCreateScreen"))
+        self.continueButton = ttk.Button(self, text = "Continue", command = self.analyzeData)
+        self.selectDataButton = ttk.Button(self, text = "Upload", command = self.selectFiles)
+        self.label = ttk.Label(self, background = "gray")
+
+    def selectFiles(self):
+        try:
+            self.files = filedialog.askopenfilenames(parent = self, title = "Choose Data")
+            if len(self.files) > 1:
+                self.label.configure(text = (self.files[0][:20]) + "...+ "+ str(len(self.files)-1) + " more" )
+                return
+            self.label.configure(text = (self.files[0][:20])+"...")
+            self.continueButton.grid(column = 3, row = 3, sticky = "nsew", padx = 10, pady = 10)
+        except:
+            self.label.configure(text = "Could Not Select Files")
+            self.continueButton.grid_remove()
+
+    def analyzeData(self):
+        for x in (0, len(self.files)-1):
+            backend.analyze_sample(self.controller.selectedProfile, self.files[x])
+        self.controller.showFrame("GoHomeScreen")
 
     def configureRowsColumns(self):
         # resize configs
@@ -24,15 +48,18 @@ class UploadDataScreen(tk.Frame):
         self.parent.rowconfigure(0, weight = 1)
 
         self.columnconfigure(0, weight = 1)
-        self.columnconfigure(1, weight = 5)
-        self.columnconfigure(2, weight = 1)
-        self.rowconfigure(0, weight = 3)
+        self.columnconfigure(1, weight = 1)
+        self.columnconfigure(2, weight = 4)
+        self.columnconfigure(3, weight = 1)
+        self.rowconfigure(0, weight = 10)
         self.rowconfigure(1, weight = 1)
-        self.rowconfigure(2, weight = 2)
+        self.rowconfigure(2, weight = 9)
         self.rowconfigure(3, weight = 1)
 
 
     def placeWidgets(self):
         self.backButton.grid(column = 0, row = 3, sticky = "nsew", padx = 10, pady = 10)
+        self.selectDataButton.grid(column = 1, row = 1, sticky = "nsew", padx = 1, pady = 10)
+        self.label.grid(column = 2, row = 1, sticky = "nsew", padx = 1, pady = 10)
 
         
